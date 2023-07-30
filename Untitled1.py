59fcf01c50ccf80adc85e344831c6771c571820b
fullto <140895105+fullto@users.noreply.github.com>
2023-07-30 16:17:37 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("hello data science")
+
+
+# In[5]:
+
+
+True and False
+
+
+# In[9]:
+
+
+True or False
+
+
+# In[11]:
+
+
+True and True
+
+
+# In[13]:
+
+
+not True
+
+
+# In[14]:
+
+
+not False
+
+
+# In[15]:
+
+
+True is True
+
+
+# In[17]:
+
+
+True is False
+
+
+# In[20]:
+
+
+'a' is 'a'
+
+
+# In[21]:
+
+
+True + True
+
+
+# In[22]:
+
+
+True + False
+
+
+# In[23]:
+
+
+False + False
+
+
+# In[24]:
+
+
+False - True
+
+
+# In[25]:
+
+
+10==10
+
+
+# In[27]:
+
+
+10 <=10
+
+
+# In[28]:
+
+
+True and False or True
+
+
+# In[29]:
+
+
+x=1
+type(x)
+
+
+# In[31]:
+
+
+name1='xyz'
+type(name1)
+
+
+# In[32]:
+
+
+tenth=2
+tenth
+
+
+# In[33]:
+
+
+x=10.2
+type(x)
+
+
+# In[ ]:
+
+
+
+
